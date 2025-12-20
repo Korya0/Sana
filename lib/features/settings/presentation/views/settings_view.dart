b940a25d150e38bad3919d6a1101d5464b659775
Korya25 <????? ??? GitHub>
2025-12-21 01:59:57 +0200

    feat: add settings view with preferences, help options, and social media links

--- lib/features/settings/presentation/views/settings_view.dart
+++ lib/features/settings/presentation/views/settings_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:sana/core/common/widgets/common_sliver_app_bar.dart';
-import 'package:sana/core/common/widgets/smart_support_card.dart';
 import 'package:sana/core/constants/app_spacing.dart';
 import 'package:sana/features/settings/presentation/widgets/help_options_section.dart';
 import 'package:sana/features/settings/presentation/widgets/preferences_section.dart';
@@ -28,10 +27,6 @@ class SettingsView extends StatelessWidget {
               const SizedBox(height: 24),
 
               const SizedBox(height: AppSpacing.betweenSections18),
-
-              // heree
-              const SmartSupportCard(),
-              const SizedBox(height: 24),
 
               const SocialMediaLinks(),
               const SizedBox(height: 24),
