92f2d8d6597a0843316860b98df540d12b80343d
kareem zedan <kareemzedan@kareems-iMac-Pro.local>
2026-03-17 21:06:58 +0200

    chore: minor iOS updates for App Store submission

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.sana.muslim.app"
     compileSdk = 36  
-    ndkVersion = "28.2.13676358"
-
+    ndkVersion = "27.0.12077973"
+ 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -42,23 +42,11 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
-    signingConfigs {
-        create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = keystoreProperties["storeFile"]?.let { File(it.toString()) }
-            storePassword = keystoreProperties["storePassword"] as String
-        }
+     
+  buildTypes {
+    release {
     }
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now,
-            // so `flutter run --release` works.
-            //signingConfig = signingConfigs.getByName("debug")
-            signingConfig = signingConfigs.getByName("release")
-        }
-    }
+}
 
     packaging {
         jniLibs {
