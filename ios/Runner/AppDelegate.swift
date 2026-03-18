c5c31101180bf2f0faf94f784a128269bf7d7f14
mahmoudmohamed <mahmoudmohamed01559@gmail.com>
2026-03-18 16:21:46 +0200

    feat: Implement AppDelegate to configure audio session, integrate Crashlytics, and register Flutter plugins.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,3 +1,5 @@
+import AVFoundation
+import FirebaseCrashlytics
 import Flutter
 import UIKit
 
@@ -7,10 +9,25 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    let result = super.application(application, didFinishLaunchingWithOptions: launchOptions)
+
+    do {
+      try AVAudioSession.sharedInstance().setCategory(
+        .playback,
+        mode: .default,
+        options: [.mixWithOthers]
+      )
+      try AVAudioSession.sharedInstance().setActive(true)
+    } catch {
+      Crashlytics.crashlytics().record(error: error)
+    }
+
+    return result
   }
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 }
+
+
