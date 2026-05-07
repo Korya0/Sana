fcdabd6c16cc740164a6e2a235da1e030b9c0c63
mahmoudmohamed <mahmoudmohamed01559@gmail.com>
2026-05-07 14:41:45 +0300

    chore: secure project by removing sensitive files from tracking and adding github actions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     id("com.android.application")
     // START: FlutterFire Configuration
     id("com.google.gms.google-services")
+    id("com.google.firebase.firebase-perf")
     id("com.google.firebase.crashlytics")
     // END: FlutterFire Configuration
     id("kotlin-android")
@@ -23,7 +24,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.sana.muslim.app"
     compileSdk = 36  
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
  
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\dev\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\flutter\flutter_Projects\muslim_app\sana_app"
+export "FLUTTER_APPLICATION_PATH=D:\flutter\flutter_Projects\muslim_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.1.0"
-export "FLUTTER_BUILD_NUMBER=5"
+export "FLUTTER_BUILD_NUMBER=6"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
